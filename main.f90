9c5327782df3e52d0721e2427af20c6df5d669b8
Regeraa <82404669+Regeraa@users.noreply.github.com>
2021-11-22 21:21:19 +0100

    adding module

--- main.f90
+++ main.f90
@@ -1,4 +1,12 @@
+module projet
+    implicit none
+    
+contains
+    
+end module projet
+
 program projet_esteban_nemo
+    use projet
     implicit none
     print *, "Hello world"  
 end program projet_esteban_nemo
