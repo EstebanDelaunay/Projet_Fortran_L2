c87423a6229158153b1b6785313bfd4b8c68bd54
Regeraa <82404669+Regeraa@users.noreply.github.com>
2021-11-22 23:27:27 +0100

    Mise en place du corp du programme

--- main.f90
+++ main.f90
@@ -1,12 +1,52 @@
-module projet
-    implicit none
+module projet!! variable principale
     
-contains
+    type tas 
+        !! Type dérivé
+        INTEGER :: rayon 
+            !! Nombre de piles
+        INTEGER :: hmax 
+            !! Hauteur max du tas de sable
+        INTEGER, DIMENSION(:), ALLOCATABLE :: pile 
+            !! Tableau du nombre de grains par pile
+        CHARACTER(1), DIMENSION(:,:), ALLOCATABLE :: grille 
+            !! Tableau déstiné à l'affichage du tas de sable
+    end type tas
     
+    contains
+
+    function lecture_controlee(nmin, nmax) result(valeur)
+        !! Retourne une valeur saisie par l'utilisateur 
+        !! en s'assurant qu'elle est comprise entre nmin et nmax
+
+        implicit none
+        integer :: nmin, nmax
+        integer :: valeur
+
+        !/**********************/
+        !* Corps de la fonction *
+        !/**********************/
+        
+    end function lecture_controlee
+
+    subroutine affiche(un_tas) 
+        !! Affiche l'état du tas
+        implicit none
+        type(tas),intent(in) :: un_tas
+
+        !/************************/
+        !* Corps de la subroutine *
+        !/************************/
+    
+    end subroutine affiche
 end module projet
 
 program projet_esteban_nemo
     use projet
     implicit none
-    print *, "Hello world"  
+    TYPE(tas) :: mon_tas
+    
+    !/********************/
+    !* Corps du programme *
+    !/********************/
+
 end program projet_esteban_nemo
