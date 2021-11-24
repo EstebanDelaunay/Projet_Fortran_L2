54fef9c913a1f4afec280a771e85fa8987b8f60c
Regeraa <82404669+Regeraa@users.noreply.github.com>
2021-11-24 22:59:13 +0100

    Affichage Basique

--- main.f90
+++ main.f90
@@ -1,5 +1,3 @@
-!! ESTEBAN BRANCH
-
 module projet
 
     type tas 
@@ -19,42 +17,42 @@ module projet
     INTEGER function lecture_controlee(nmin, nmax)
         !! Retourne une valeur saisie par l'utilisateur 
         !! en s'assurant qu'elle est comprise entre nmin et nmax
-
         implicit none
         integer, INTENT(IN) :: nmin, nmax
-        integer :: valeur
-
-        valeur = 0
+        integer :: valeur = 0
 
         do
             print*, "Entrez une valeur comprise entre : ", nmin, "et", nmax
             read*, valeur
-
             if (valeur > nmin .AND. valeur < nmax) exit 
-
         end do
-
         lecture_controlee = valeur
-        
+
     end function lecture_controlee
 
     subroutine affiche(un_tas) 
         !! Affiche l'état du tas
         implicit none
         type(tas),intent(in) :: un_tas
-
-        !/************************/
-        !* Corps de la subroutine *
-        !/************************/
+        INTEGER :: line , colonne
+        !CHARACTER, DIMENSION(un_tas%hmax,un_tas%rayon) :: tab
+        
+        do line = 1, un_tas%hmax
+            do colonne = 1, (un_tas%rayon)
+                write(*, fmt="(1x,a,i0)",advance= "no") un_tas%grille(line,colonne)
+            end do
+            print *,""
+        end do
+        
     
     end subroutine affiche
 
-    subroutine transfert_grain(pile)
-        !! Modifie le tableau pile pour simuler la chute des grains
-        INTEGER, DIMENSION(:), intent(inout) :: pile
-
-
-    end subroutine transfert_grain
+    !subroutine transfert_grain(pile)
+    !    !! Modifie le tableau pile pour simuler la chute des grains
+    !    INTEGER, DIMENSION(:), intent(inout) :: pile
+    !
+    !
+    !end subroutine transfert_grain
 
 end module projet
 
@@ -63,21 +61,35 @@ program projet_esteban_nemo
     implicit none
     TYPE(tas) :: mon_tas
     integer, PARAMETER :: borne_inf = 3, borne_sup = 40, nt = 10
-    INTEGER :: i = 0
+    !INTEGER :: i = 0
+    INTEGER :: ok
+    
+    ! /**/ Demande à l'user les valeurs du rayon et la hauteur /**/
+    !print *, "Rentrez le rayon maximum du tas" 
+    mon_tas%rayon = 10
+    !mon_tas%rayon = lecture_controlee(borne_inf, borne_sup)
+    !print *, "Rentrez la hauteur maximum du tas"
+    !mon_tas%hmax = lecture_controlee(borne_inf, borne_sup)
+    mon_tas%hmax = 5
 
-    mon_tas%rayon = lecture_controlee(borne_inf, borne_sup)
-    mon_tas%hmax = lecture_controlee(borne_inf, borne_sup)
+    ALLOCATE ( mon_tas%pile(0:(mon_tas%rayon - 1)) , mon_tas%grille(mon_tas%hmax,mon_tas%rayon ) , stat = ok) 
+    IF (ok /= 0) STOP "Problème allocation !" 
 
-    ALLOCATE(mon_tas%pile(0 : mon_tas%rayon - 1), mon_tas%grille(mon_tas%rayon,mon_tas%hmax))
+    mon_tas%grille = "0" ; mon_tas%grille(1,2:) = " " ; mon_tas%grille(2,3:) = " " ; mon_tas%grille(3,4:) = " "
+    mon_tas%grille(4,6:) = " " ; mon_tas%grille(5,8:) = " "
 
-    do
-        if (maxval(mon_tas%pile) >= mon_tas%hmax) exit 
-            !! Quitte la boucle si la hauteur max est atteinte
-        if (mod(i,nt) /= 0) mon_tas%pile(0) = mon_tas%pile(0) + 1 
-            !! Ajout d'un grain tout les nt
-        call transfert_grain(mon_tas%pile)
-        call affiche(mon_tas)
-        i = i + 1
-    end do
+    call affiche(mon_tas)
 
+    !do
+    !    if (maxval(mon_tas%pile) >= mon_tas%hmax) exit 
+    !        !! Quitte la boucle si la hauteur max est atteinte
+    !    if (mod(i,nt) /= 0) mon_tas%pile(0) = mon_tas%pile(0) + 1 
+    !        !! Ajout d'un grain tout les nt
+    !    call transfert_grain(mon_tas%pile)
+    !    call affiche(mon_tas)
+    !    i = i + 1
+    !end do
+
+
+    DEALLOCATE (mon_tas%pile, mon_tas%grille)
 end program projet_esteban_nemo
